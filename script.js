13507758c8da01a4bc06d657881706fc34ce888e
Ashutosh <ashutosh@alethea.in>
2023-08-17 18:04:55 +0530

    bug fix for scroll on mobile view

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ const menuItemsDiv = document.getElementById('menu-items');
 menu.addEventListener('click', ()=>{
     // alert('open')
     menuItemsDiv.classList.remove("right-[100%]");
+    menuItemsDiv.classList.remove('absolute')
+    menuItemsDiv.classList.add('fixed')
+
     // menu.classList.remove('bg-orange-300')
     // close-menu
 })
 
 closeMenuBtn.addEventListener('click', ()=>{
     menuItemsDiv.classList.add("right-[100%]");
+    menuItemsDiv.classList.remove('fixed')
+    menuItemsDiv.classList.add('absolute')
     // alert('closed')
     // close-menu
 })
